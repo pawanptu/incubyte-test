a76185f8712e37e81b5dc4652414c6a879433643
Pawan kumar <pawan@squadsy.team>
2024-05-28 20:05:18 +0530

    handle negative with exception

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,28 @@
 class StringCalculator
     def self.add(numbers)
-        return 0 if numbers.empty?
-        return numbers.to_i if numbers.to_i.to_s == numbers
-        numbers.split(/[\n,]/).map(&:to_i).sum
+      #For empty input
+      return 0 if numbers.empty?
+
+      # For single number
+      return numbers.to_i if numbers.to_i.to_s == numbers
+  
+      # delimiter of comma and new line
+      delimiter = /,|\n/
+
+      # Checking for delimiter
+      if numbers.start_with?("//")
+        parts = numbers.split("\n", 2)
+        delimiter = Regexp.escape(parts[0][2])
+        numbers = parts[1]
+      end
+
+      # Split the string into numbers array
+      num_list = numbers.split(/#{delimiter}/).map(&:to_i)
+      negatives = num_list.select { |n| n < 0 }
+      raise "negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+  
+      # Return sum
+      num_list.sum
     end
 end
+  
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -21,9 +21,24 @@ class StringCalculatorTest < Minitest::Test
         assert_equal 6, StringCalculator.add("1,2,3")
         assert_equal 10, StringCalculator.add("2,3,5")
     end   
-    
+
     def test_new_lines_between_numbers
         assert_equal 6, StringCalculator.add("1\n2,3")
         assert_equal 10, StringCalculator.add("2,3\n5")
     end   
+
+    def test_different_delimiters
+        assert_equal 3, StringCalculator.add("//;\n1;2")
+        assert_equal 6, StringCalculator.add("//|\n1|2|3")
+    end   
+    
+    def test_negative_numbers
+        assert_raises(RuntimeError, "negative numbers not allowed: -1") do
+          StringCalculator.add("1,-1")
+        end
+    
+        assert_raises(RuntimeError, "negative numbers not allowed: -1,-3") do
+          StringCalculator.add("1,-1,2,-3")
+        end
+    end
 end
